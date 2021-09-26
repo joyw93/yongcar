3a2b7394d289c4dde4d8aec8c3cee81ed0c275ef
unknown <joyw93@naver.com>
2021-09-26 17:38:33 +0900

    test

--- yong/filter.py
+++ yong/filter.py
@@ -1,5 +1,5 @@
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
-def format_datetime(value, fmt='%Y년 %m월 %d일 %H:%M'):
+def format_datetime(value, fmt='%Y-%m-%d %H:%M'):
     return value.strftime(fmt)
