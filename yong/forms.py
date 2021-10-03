63222eedc4b9aa74f7da649146160258e86cd194
unknown <joyw93@naver.com>
2021-10-03 16:47:12 +0900

    loginform-2차수정

--- yong/forms.py
+++ yong/forms.py
@@ -7,11 +7,11 @@ class RegistrationForm(FlaskForm):
     name = StringField("이름",
                        validators=[DataRequired('이름을 입력하세요.'), Length(min=2, max=5, message='이름이 적절하지 않습니다.')])
     email = StringField("이메일",
-                        validators=[DataRequired('메일주소를 입력하세요.'), Email('이메일 형식이 아닙니다.')])
+                        validators=[DataRequired('메일주소를 입력하세요.'), Email('이메일 형식이 아닙니다.'), Length(min=4, max=30, message='이메일 형식이 적절하지 않습니다.')])
     pw = PasswordField("비밀번호",
-                       validators=[DataRequired('비밀번호를 입력하세요.'), Length(min=4, max=30, message='비밀번호 형식이 적절하지 않습니다.')])
+                       validators=[DataRequired('비밀번호를 입력하세요.'), Length(min=4, max=100, message='비밀번호가 너무 짧습니다. (최소 4자 이상)')])
     pw_confirm = PasswordField("비밀번호 확인",
-                       validators=[DataRequired('비밀번호를 입력하세요.'), EqualTo("pw", message='비밀번호가 일치하지 않습니다.')])
+                       validators=[DataRequired('확인용 비밀번호를 입력하세요.'), EqualTo("pw", message='비밀번호가 일치하지 않습니다.')])
     submit = SubmitField("가입")
 
 
