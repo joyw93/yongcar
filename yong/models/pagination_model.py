84d823eefea69894a34d9c63de87121cf9ced041
unknown <joyw93@naver.com>
2021-10-06 13:52:17 +0900

    페이지네이션 수정

--- yong/models/pagination_model.py
+++ yong/models/pagination_model.py
@@ -1,4 +1,7 @@
-
+import math
 class Pagination:
-    def __init__(self):
-        self.temp = 0
+    def __init__(self, list_size, page_size):
+        self.current_page = 1
+        self.list_size = list_size
+        self.page_size = page_size
+        
