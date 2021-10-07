1ba191b62d1403524004ab9b88871bf8a9b8434b
unknown <joyw93@naver.com>
2021-10-07 15:46:35 +0900

    csv DB연동 1차구현

--- yong/views/test_view.py
+++ yong/views/test_view.py
@@ -12,28 +12,13 @@ bp = Blueprint('test', __name__, url_prefix='/test')
 def form():
     
     
-    list_size = 5
-    question_size = Question.get_size()
-    page_count = math.ceil(question_size/list_size)
     
-
-    question_list = Question.get_page(0,5)
     
-    return render_template('test_form.html', question_list=question_list, page_count=page_count)
+    return render_template('test_form.html')
 
 
-@bp.route('/result/<int:page>')
-def result(page):
-    pagination = Pagination()
-    list_size = 5
-    question_size = Question.get_size()
-    page_count = math.ceil(question_size/list_size)
-    question_list = Question.get_page(page*list_size,list_size)    
-    question_list = Question.get_list()
-    
-    # if (page%3 == 0) & (page>0):
-    #     pagination.temp = pagination.temp+3
-    #     return render_template('test_result.html',question_list=question_list, page_count=page_count, page=page, temp=pagination.temp)
+@bp.route('/result')
+def result():
    
     
-    return render_template('test_result.html',question_list=question_list, page_count=page_count, page=page, temp=pagination.temp)
+    return render_template('test_result.html')
